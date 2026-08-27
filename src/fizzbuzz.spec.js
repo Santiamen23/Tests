dfcdb2727b8f3a7020a2cc980f286e3a1ca0c316
Santiamen23 <santymenber@gmail.com>
2026-08-27 09:53:01 -0400

    feat: tercer ciclo de prueba 97 deberia devolver 97

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,10 +1,14 @@
+import fizzbuzz from "./fizzbuzz.js"
 
 describe("FizzBuzz",()=>{
-  it("deberia imprimir 2",()=>{
+  it("deberia imprimir el mismo numero si no sigue la regla",()=>{
     expect(fizzbuzz(2)).toEqual("2");
+  })
+  it("deberia imprimir otro numero que no sigue la regla",()=>{
+    expect(fizzbuzz(2)).toEqual("2");
+  })
+    it("deberia imprimir el mismo numero si no sigue la regla",()=>{
+    expect(fizzbuzz(97)).toEqual("97");
   })
 })
 
-function fizzbuzz(){
-  return "2";
-}
