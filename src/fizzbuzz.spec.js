3d6b812962976ce815cef4ce3451c5ae41d50e1c
Santiamen23 <santymenber@gmail.com>
2026-08-27 09:58:29 -0400

    feat: septimo ciclo de prueba 10 deberia devolver Buzz

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,17 +7,20 @@ describe("FizzBuzz",()=>{
   it("deberia imprimir otro numero que no sigue la regla",()=>{
     expect(fizzbuzz(2)).toEqual("2");
   })
-    it("deberia imprimir el mismo numero si no sigue la regla",()=>{
+  it("deberia imprimir el mismo numero si no sigue la regla",()=>{
     expect(fizzbuzz(97)).toEqual("97");
   })
-    it("deberia imprimir otro numero que no sigue la regla",()=>{
+  it("deberia imprimir otro numero que no sigue la regla",()=>{
     expect(fizzbuzz(8)).toEqual("8");
   })
-      it("deberia imprimir Fizz",()=>{
+  it("deberia imprimir Fizz",()=>{
     expect(fizzbuzz(33)).toEqual("Fizz");
   })
   it("deberia imprimir Fizz",()=>{
     expect(fizzbuzz(6)).toEqual("Fizz");
+  })
+  it("deberia imprimir Buzz",()=>{
+    expect(fizzbuzz(10)).toEqual("Buzz");
   })
 })
 
